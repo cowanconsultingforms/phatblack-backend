ecca03f670ed58051c78a609d2e32a286e6df2ec
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-03-20 23:33:54 -0400

    deleting users works

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,35 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({ origin: true });
 
-admin.initializeApp();
+// Initialize Firebase Admin with service account
+const serviceAccount = require("../serviceKey.json");
 
-exports.receiveMessage = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    if (request.method !== 'POST') {
-      return response.status(405).send('Method Not Allowed');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://phat-black-default-rtdb.firebaseio.com"
+});
+
+// Cloud Function to delete a user
+exports.deleteUser = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    if (req.method !== 'DELETE') {
+      return res.status(405).send({ error: 'Method Not Allowed' });
     }
 
-    console.log(request.body.message);
+    try {
+      const { userId } = req.body;
 
-    response.send({result: `Message received: ${request.body.message}`});
+      if (!userId) {
+        return res.status(400).send({ error: "User ID is required" });
+      }
+
+      await admin.auth().deleteUser(userId);
+
+      res.status(200).send({ result: `User with ID ${userId} deleted successfully` });
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      res.status(500).send({ error: error.message });
+    }
   });
 });
