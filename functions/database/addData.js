20aa7acee465080e53c7fd8478d69ef95458fe47
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-21 09:19:28 -0400

    added comments for addData

--- functions/database/addData.js
+++ functions/database/addData.js
@@ -1,7 +1,7 @@
 const SearchData = require("./searchModel");
 
 const addData = async (data) => {
-  // Create a new document with the data
+  // Create a new document with the data, like creating a new instance of a class
   const searchData = new SearchData(data);
   // Save the document
   await searchData.save();
